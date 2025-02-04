44eeaa84b49f50ee031bc40788c1d9f984853626
SuzukiShuntarou <da8801593@gmail.com>
2025-02-04 21:21:28 +0900

    開発環境でメール送信可能な設定の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
   def after_sign_out_path_for(_resource)
     new_user_session_path
   end
