a83b25b5abd0eb066c0f7faf7a8ea38124a69560
SuzukiShuntarou <da8801593@gmail.com>
2025-02-04 17:35:55 +0900

    i18nのデフォルトをjaに変更

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module GifTreat
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
   end
 end
