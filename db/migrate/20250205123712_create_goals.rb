d408be8f017422abfaed6a05a5162e33f8b31022
SuzukiShuntarou <da8801593@gmail.com>
2025-02-05 21:41:31 +0900

    Goalsモデルの作成

---
+++ db/migrate/20250205123712_create_goals.rb
@@ -0,0 +1,14 @@
+class CreateGoals < ActiveRecord::Migration[7.2]
+  def change
+    create_table :goals do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :reward, null: false, foreign_key: true
+      t.string :description, null: false
+      t.integer :progress, null: false, default: 0 
+      t.integer :likings_count, null: false, default: 0 
+      t.integer :cheerings_count, null: false, default: 0 
+
+      t.timestamps
+    end
+  end
+end
