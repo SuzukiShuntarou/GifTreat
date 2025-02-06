be734d6544f89b2417c688a165163b8440b0872e
SuzukiShuntarou <da8801593@gmail.com>
2025-02-06 10:03:08 +0900

    Rewardコントローラーとビューの作成

---
+++ app/controllers/rewards_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class RewardsController < ApplicationController
+  def show; end
+
+  def new; end
+
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :rewards, only: [:show, :new, :create, :edit, :update, :destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener" 
