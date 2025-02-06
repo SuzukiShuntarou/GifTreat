b61deeac3a00ebb10e79e9cabe19f381d05d5378
SuzukiShuntarou <da8801593@gmail.com>
2025-02-06 10:18:10 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "goals/index"
-  get "goals/edit"
+  root to: redirect('/goals')
+
   devise_for :users
   resources :rewards, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :goals, only: [:index, :edit, :update]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener" 
