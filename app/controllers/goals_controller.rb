3c5299523a4d17a07704353de46819ae42433325
SuzukiShuntarou <da8801593@gmail.com>
2025-02-06 13:34:14 +0900

    完了状態かどうかで表示内容を変えるメソッドの追加とindexでの利用

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class GoalsController < ApplicationController
-  def index; end
+  def index
+    @goals = Goal.search_rewards_completed_or_in_progress(params[:display], current_user)
+  end
 
   def edit; end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -8,4 +8,15 @@ class Goal < ApplicationRecord
 
   validates :description, presence: true
   validates :progress, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, only_integer: true }
+
+  def self.search_rewards_completed_or_in_progress(display, current_user)
+    goals = Goal.includes(:reward)
+                .where(user_id: current_user.id)
+                .order('rewards.completiondate ASC, rewards.id ASC')
+    if display == 'completed'
+      goals.where(rewards: { completiondate: ...Date.current }).reverse
+    else
+      goals.where(rewards: { completiondate: Date.current.. })
+    end
+  end
 end
