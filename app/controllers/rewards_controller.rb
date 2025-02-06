fe4f31cf0d7b47757af37a06549367021bd128d9
SuzukiShuntarou <da8801593@gmail.com>
2025-02-06 23:43:33 +0900

    Rewardの編集、削除をモーダルで実装

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class RewardsController < ApplicationController
-  def show; end
+  before_action :set_reward, only: %i[edit update destroy]
+
+  def show
+    reward_id = params[:id]
+    groups = RewardParticipant.includes(reward: { goals: :user }).where(user: current_user)
+    @reward = groups.find_by!(reward_id:).reward
+    @goals = @reward.goals
+  end
 
   def new
     @reward = Reward.new
@@ -24,9 +31,30 @@ class RewardsController < ApplicationController
     end
   end
 
+  def update
+    if @reward.in_progress? && @reward.update(reward_params)
+      flash.now[:updated_reward_notice] = 'ご褒美の編集に成功！'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reward.destroy! if @reward.in_progress?
+    redirect_to goals_path, notice: 'ご褒美の削除に成功！'
+  end
+
   private
 
   def reward_and_goal_params
     params.require(:reward).permit(:completion_date, :description, :location, goals_attributes: %i[description progress])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:completion_date, :description, :location)
+  end
+
+  def set_reward
+    @reward = current_user.reward_participants.find_by!(reward_id: params[:id]).reward
   end
 end
