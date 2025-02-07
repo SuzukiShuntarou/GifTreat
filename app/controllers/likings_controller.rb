8f0d05055addd98bac7f1f84d67db7e788554dfc
SuzukiShuntarou <da8801593@gmail.com>
2025-02-07 17:45:51 +0900

    いいね機能の実装

--- app/controllers/likings_controller.rb
+++ app/controllers/likings_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class LikingsController < ApplicationController
+  def create
+    @goal = Goal.find(params[:goal_id])
+    @liking = @goal.likings.build(user: current_user)
+    @liking.save!
+  end
 end
--- app/helpers/likings_helper.rb
+++ app/helpers/likings_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module LikingsHelper
+  def generate_likings_message(likings)
+    user_names = likings.joins(:user).pluck('users.name').uniq
+
+    if user_names.empty?
+      '頑張る人にひと押し、いいねボタンでさりげなく褒めてみて！'
+    else
+      "#{user_names.join('と')}が褒めてくれたよ！"
+    end
+  end
 end
