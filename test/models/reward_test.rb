431c10806fd66140834900f66a7dae2a1ad9f568
SuzukiShuntarou <da8801593@gmail.com>
2025-02-07 15:26:46 +0900

    招待機能とRewardParticipantsの作成に関するRewardモデルのテストを追加

--- test/models/reward_test.rb
+++ test/models/reward_test.rb
@@ -5,6 +5,7 @@ require 'test_helper'
 class RewardTest < ActiveSupport::TestCase
   setup do
     @current_user = users(:alice)
+    @other_user = users(:bob)
     @reward_in_progress = rewards(:alice_reward_in_progress)
     @reward_completed = rewards(:alice_reward_completed)
   end
@@ -15,5 +16,29 @@ class RewardTest < ActiveSupport::TestCase
 
   test 'should not be in progress' do
     assert_not @reward_completed.in_progress?
+  end
+
+  test 'should be created associated with User and Reward, RewardParticipant' do
+    reward = Reward.create(
+      completion_date: Date.current.to_s,
+      location: '北海道',
+      description: '旅行',
+      invitation_token: SecureRandom.urlsafe_base64
+    )
+    assert_not RewardParticipant.find_by(user: @current_user, reward: reward).present?
+    reward.create_reward_participants(@current_user)
+    assert_predicate RewardParticipant.find_by(user: @current_user, reward: reward), :present?
+  end
+
+  test 'should be created associated with other_user and reward when invited, RewardParticipant' do
+    assert_not RewardParticipant.find_by(user: @other_user, reward: @reward_in_progress).present?
+    @reward_in_progress.create_reward_participants(@other_user)
+    assert_predicate RewardParticipant.find_by(user: @other_user, reward: @reward_in_progress), :present?
+  end
+
+  test 'should have initial Goal created associated with other_user and reward when invited' do
+    assert_not Goal.find_by(user: @other_user, reward: @reward_in_progress).present?
+    @reward_in_progress.create_initial_goal_by_invited(@other_user)
+    assert_predicate(Goal.find_by(user: @other_user, reward: @reward_in_progress), :present?)
   end
 end
