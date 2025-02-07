02c282d6e54fa534d4ce980dbe9c54322877eaa3
SuzukiShuntarou <da8801593@gmail.com>
2025-02-07 18:00:07 +0900

    応援機能の追加

--- app/controllers/cheerings_controller.rb
+++ app/controllers/cheerings_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class CheeringsController < ApplicationController
+  def create
+    @goal = Goal.find(params[:goal_id])
+    @cheering = @goal.cheerings.build(user: current_user)
+    @cheering.save!
+  end
 end
--- app/helpers/cheerings_helper.rb
+++ app/helpers/cheerings_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module CheeringsHelper
+  def generate_cheerings_message(cheerings)
+    user_names = cheerings.joins(:user).pluck('users.name').uniq
+
+    if user_names.empty?
+      'まだ静かな時間が流れているね。応援で流れを変えてみては？'
+    else
+      "#{user_names.join('と')}が応援しているよ！"
+    end
+  end
 end
