21a43de8402e6eeb1c84acfea67c56db3e3db6b9
SuzukiShuntarou <da8801593@gmail.com>
2025-02-08 16:34:46 +0900

    招待用のボタンが最大人数になると表示されないように修正

--- app/helpers/rewards_helper.rb
+++ app/helpers/rewards_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module RewardsHelper
+  MAX_REWARD_RELATED_GOALS = 4
+
+  def invited?(reward)
+    reward.in_progress? && reward.goals.count < MAX_REWARD_RELATED_GOALS
+  end
 end
