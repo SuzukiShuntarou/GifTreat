b812deaf868d60150030352a9f066b161edf1382
SuzukiShuntarou <da8801593@gmail.com>
2025-02-15 11:16:07 +0900

    目標の最大文字数を140文字とするバリデーションを追加

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -8,7 +8,7 @@ class Goal < ApplicationRecord
   has_many :likings, dependent: :destroy
   has_many :cheerings, dependent: :destroy
 
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 140 }
   validates :progress, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, only_integer: true }
   validate :validate_reward_related_goals_limit, on: :create
   validate :validate_in_progress, on: :update
