d39ba204b82941adf7d1267a214ab4ad445c607a
SuzukiShuntarou <da8801593@gmail.com>
2025-02-17 15:28:16 +0900

    進捗率のスライダーでの入力機能の実装

---
+++ app/javascript/controllers/slider_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="slider"
+export default class extends Controller {
+  connect() {
+    this.updateButtons = document.getElementById("update_buttons");
+  }
+
+  inputChange() {
+    this.updateHiddenInput();
+    this.showUpdateButtons();
+  }
+
+  updateHiddenInput() {
+    const hiddenInputProgress = document.getElementById(
+      "hidden_input_progress",
+    );
+    hiddenInputProgress.value = this.element.value;
+  }
+
+  showUpdateButtons() {
+    this.updateButtons.style.display = "flex";
+  }
+
+  close() {
+    this.updateButton.style.display = "none";
+  }
+}
