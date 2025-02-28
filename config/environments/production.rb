5d1bdd3d101cafda4bd90dd6593301846107e6df
SuzukiShuntarou <da8801593@gmail.com>
2025-02-28 11:11:07 +0900

    fromに表示される内容の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,6 +102,7 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_mailer.default_url_options = { host: 'giftreat.onrender.com'}
+  action_mailer.default_options = { from: 'no-reply@giftreat.jp' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
