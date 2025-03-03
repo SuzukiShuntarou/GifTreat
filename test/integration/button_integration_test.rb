ede71a147a79d972fbdfc11d5ae469f8157cd0fd
SuzukiShuntarou <da8801593@gmail.com>
2025-03-03 21:23:15 +0900

    ボタンヘルパーメソッドの命名を変更した

--- test/integration/button_integration_test.rb
+++ test/integration/button_integration_test.rb
@@ -6,11 +6,11 @@ class ButtonHelperTest < ActionDispatch::IntegrationTest
   include ButtonHelper
   test 'should be active only compatible button when accessed in-progress or completed goals-path' do
     get goals_path(rewards: 'inprogress')
-    assert_equal 'active', current_page_button_or_not('inprogress')
-    assert_equal '', current_page_button_or_not('completed')
+    assert_equal 'active', active_or_not('inprogress')
+    assert_equal '', active_or_not('completed')
 
     get goals_path(rewards: 'completed')
-    assert_equal '', current_page_button_or_not('inprogress')
-    assert_equal 'active', current_page_button_or_not('completed')
+    assert_equal '', active_or_not('inprogress')
+    assert_equal 'active', active_or_not('completed')
   end
 end
