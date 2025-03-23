2f754ad123ab059889a3442d35fe0d11486b7e1d
SuzukiShuntarou <da8801593@gmail.com>
2025-03-23 10:01:03 +0900

    招待のURLをinviteからinvitationへ変更した

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   }
   resources :rewards, only: [:show, :new, :create, :edit, :update, :destroy] do
     member do
-      get 'invite'
+      get 'invitation'
     end
   end
   resources :goals, only: [:index, :edit, :update] do
